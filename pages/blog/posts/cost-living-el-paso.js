4512147962e748c26c602d4737ac09dfda33a2ad
John Pena <pemu775@gmail.com>
2020-10-26 14:39:48 -0600

    Trying to find missing , {

--- pages/blog/posts/cost-living-el-paso.js
+++ pages/blog/posts/cost-living-el-paso.js
@@ -43,19 +43,19 @@ const Post = () => (
 
         <div className={BlogStyles.postTitle}>
           <h1>The Cost of Living in El Paso Texas</h1>
-          <span>September 7, 2020</span>
           <ArticleJsonLd
             url='https://penaelpaso.com/blog/posts/cost-living-el-paso'
             title="Cost of Living in El Paso Texas"
             images={[
               'https://res.cloudinary.com/mimas-music/image/upload/v1603729314/Living%20In%20El%20Paso%20Texas/cost-living-d.jpg',
             ]}
-            datePublished="2020-09-07"
+            datePublished="2020-09-07T08:00:00+08:00"
             authorName="John Pena"
             publisherName="Living in El Paso Texas"
             publisherLogo="https://res.cloudinary.com/mimas-music/image/upload/v1599500723/New%20Agent%20Mindset/720blackonWHITE.jpg"
             description='This video from our YouTube channel "Living in El Paso Texas" is about the unbelievable cost of living. From food to utilities to housing, you absolutely will not believe how affordable it is to live in El Paso Texas.'
           />
+          <span>September 7, 2020</span>
         </div>
 
         <p>In 2018, El Paso was ranked No. 1, as the city with the lowest cost of living by Forbes Magazine. Forbes put out a list of 13 cities where you can live comfortably on less than $60,000 a year, and El Paso was sitting right at the top!</p>
